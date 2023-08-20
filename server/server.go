c74f251781886ef7e779a0c78a2e83c2b511183c
Mark-Platts <markplattstutoring@outlook.com>
2023-08-20 17:04:51 +0100

    added functionality to server's Create and Read RPCs using storage interface

--- server/server.go
+++ server/server.go
@@ -2,41 +2,60 @@ package server
 
 import (
 	"context"
+	"log"
+
+	"markplatts.org/scrud/models"
 	pb "markplatts.org/scrud/protos"
+	"markplatts.org/scrud/storage"
 )
 
-
-
+//set up a struct that will implement the methods defined in the proto for the service, then implement methods below
 type ScrudServer struct {
 	pb.UnimplementedScrudServer
 }
 
-func (s *ScrudServer) Create(ctx context.Context, in *pb.CreateRequest) (*pb.CreateReply, error) {
-	println("Create RPC called")
-	return &pb.CreateReply{}, nil
+func (s *ScrudServer) Create(ctx context.Context, req *pb.CreateRequest) (*pb.CreateReply, error) {
+	log.Printf("Create RPC called with request: %v", req)
+
+	//use the server request to build a storage message and send it to storage layer
+	//the storage layer sends back an id to give to the server reply
+	ms := models.NewMessageStore(req.GetMessage())
+	id, err := storage.StorageMechanism.Insert(ms)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Local message store state: %v", storage.StorageMechanism.GetMessages()) //for testing/debugging TODO: remove when implementing mySQL
+	return &pb.CreateReply{Id: id}, nil
 }
 
-func (s *ScrudServer) Read(ctx context.Context, in *pb.ReadRequest) (*pb.ReadReply, error) {
-	println("Read RPC called")
-	return &pb.ReadReply{}, nil
+func (s *ScrudServer) Read(ctx context.Context, req *pb.ReadRequest) (*pb.ReadReply, error) {
+	log.Printf("Create RPC called with request: %v", req)
+
+	ms, err := storage.StorageMechanism.Retrieve(req.GetId())
+	if err != nil {
+		return nil, err
+	}	
+
+	return &pb.ReadReply{Id: req.GetId(), TimeCreated: ms.GetTimeCreated(), Message: ms.GetMessage()}, nil
 }
 
-func (s *ScrudServer) Update(ctx context.Context, in *pb.UpdateRequest) (*pb.UpdateReply, error) {
-	println("Update RPC called")
+func (s *ScrudServer) Update(ctx context.Context, req *pb.UpdateRequest) (*pb.UpdateReply, error) {
+	log.Printf("Update RPC called with request: %v", req)
 	return &pb.UpdateReply{}, nil
 }
 
-func (s *ScrudServer) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.DeleteReply, error) {
-	println("Delete RPC called")
+func (s *ScrudServer) Delete(ctx context.Context, req *pb.DeleteRequest) (*pb.DeleteReply, error) {
+	log.Printf("Delete RPC called with request: %v", req)
 	return &pb.DeleteReply{}, nil
 }
 
-func (s *ScrudServer) Send(ctx context.Context, in *pb.SendRequest) (*pb.SendReply, error) {
-	println("Send RPC called")
+func (s *ScrudServer) Send(ctx context.Context, req *pb.SendRequest) (*pb.SendReply, error) {
+	log.Printf("Send RPC called with request: %v", req)
 	return &pb.SendReply{}, nil
 }
 
-func (s *ScrudServer) SendAll(in *pb.SendAllRequest, serv pb.Scrud_SendAllServer) error {
-	println("SendAll RPC called")
+func (s *ScrudServer) SendAll(req *pb.SendAllRequest, serv pb.Scrud_SendAllServer) error {
+	log.Printf("SendAll RPC called with request: %v", req)
 	return nil
 }
